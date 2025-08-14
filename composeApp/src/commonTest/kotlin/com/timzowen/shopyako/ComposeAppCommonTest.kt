e039c1df6137eed01274e43e83c76a2797891c8a
Timz Owen <timzowen@gmail.com>
2025-08-15 01:22:47 +0300

    shop yako set up

---
+++ composeApp/src/commonTest/kotlin/com/timzowen/shopyako/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.timzowen.shopyako
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/timzowen/shopyako/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.timzowen.shopyako
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
