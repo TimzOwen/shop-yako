b645c10cc1dfac41a7be9f9328abf13f123348ef
Timz Owen <timzowen@gmail.com>
2025-08-15 14:02:41 +0300

    navigation update

--- feature/auth/src/commonMain/kotlin/com/timzowen/shopyako/component/AuthScreen.kt
+++ feature/auth/src/commonMain/kotlin/com/timzowen/shopyako/component/AuthScreen.kt
@@ -4,13 +4,20 @@ import ContentWithMessageBar
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.text.style.TextAlign
 import com.timzowen.shopyako.component.component.GoogleButtonComponent
+import com.timzowen.shopyako.shared.Alpha
 import com.timzowen.shopyako.shared.Surface
+import com.timzowen.shopyako.shared.TextPrimary
+import com.timzowen.shopyako.shared.TextSecondary
 import rememberMessageBarState
 
 @Composable
@@ -33,7 +40,23 @@ fun AuthScreen() {
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Center
                 ) {
-
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        text = "Shop Yako",
+                        textAlign = TextAlign.Center,
+                        fontSize = FontSize.EXTRA_LARGE,
+                        color = TextSecondary
+                    )
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .alpha(Alpha.HALF),
+                        text = "Sign in to continue",
+                        textAlign = TextAlign.Center,
+                        fontSize = FontSize.EXTRA_REGULAR,
+                        color = TextPrimary
+                    )
                 }
                 GoogleButtonComponent(
                     loading = false,
